a5b8e94ef1cd74b015c0dc4daa4c16f5cfab3cbe
Nikita <github@nikiv.dev>
2024-11-11 22:21:36 +0100

    add buffer

--- web/app.config.ts
+++ web/app.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "@tanstack/start/config"
 import tsConfigPaths from "vite-tsconfig-paths"
+import { nodePolyfills } from "vite-plugin-node-polyfills"
 
 export default defineConfig({
+  routers: {
+    client: {
+      vite: {
+        plugins: [
+          nodePolyfills({
+            globals: {
+              Buffer: true,
+            },
+          }),
+        ],
+      },
+    },
+  },
   vite: {
     plugins: [
       tsConfigPaths({
